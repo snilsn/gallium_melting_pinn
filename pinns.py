8b928dd7b67d140b9087e05ce2a0fe7a86fc48de
nilspf <nilspfeifer2010@googlemail.com>
2022-02-22 13:19:34 +0100

    works noe

--- pinns.py
+++ pinns.py
@@ -3,9 +3,9 @@ import tensorflow.keras as keras
 import matplotlib.pyplot as plt
 
 pinn = keras.Sequential([
-    keras.layers.Dense(100, input_shape = (1,)),
-    keras.layers.Dense(100, activation = 'tanh'),
-    keras.layers.Dense(100, activation = 'tanh'),
+    keras.layers.Dense(10, input_shape = (1,)),
+    keras.layers.Dense(10, activation = 'tanh'),
+    keras.layers.Dense(10, activation = 'tanh'),
     keras.layers.Dense(1, activation=None)
 ])
 
@@ -14,7 +14,7 @@ pinn.compile(
     loss = tf.keras.losses.MeanSquaredError()
 )
 
-t = tf.linspace(0, 20, 100)
+t = tf.linspace(0, 5, 20)
 t_input = tf.transpose(tf.concat([[t]], axis = 0))
 
 M_0 = 1.0
@@ -27,7 +27,7 @@ def analytical(M_0, l, t):
 optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
 
 start = pinn(t_input)
-for i in range(100):
+for i in range(1000):
 
     with tf.GradientTape() as tape2:
         tape2.watch([pinn.trainable_variables, t_0])
@@ -38,18 +38,17 @@ for i in range(100):
         dt = tf.cast(tape1.gradient(m, t_input), 'float32')
 
         f = dt + l*m
-        physical_loss = tf.reduce_mean(f)
+        physical_loss = tf.reduce_mean(f**2)
 
         m0 = pinn(t_0)
         initial_loss = (m0-M_0)**2
 
-    loss = physical_loss + initial_loss
-    grad = tape2.gradient(loss, pinn.trainable_variables)
+        loss = physical_loss + initial_loss
 
+    grad = tape2.gradient(loss, pinn.trainable_variables)
     optimizer.apply_gradients(zip(grad, pinn.trainable_variables))
 
-    if i % 10 == 0:
-        plt.plot(t_input, pinn(t_input), label = i+1)
+plt.plot(t_input, pinn(t_input), label = 'after training')
 
 plt.plot(t_input, start, label = 'initial guess')
 plt.plot(t_input, analytical(M_0, l, t_input), label = 'analytical solution')
